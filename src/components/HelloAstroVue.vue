67215a0e02dca962e3b776c0ad766b05687415dc
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2023-08-18 19:38:59 +0200

    Added vue and tailwind integrations

---
+++ src/components/HelloAstroVue.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1 class="text-orange-400 text-[25px]">{{ msg }}</h1>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  msg: {
+    type: String,
+    default: 'Example message',
+  },
+});
+</script>
